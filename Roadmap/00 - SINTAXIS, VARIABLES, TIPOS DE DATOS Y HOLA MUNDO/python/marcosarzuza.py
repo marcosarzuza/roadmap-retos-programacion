b7d9129a84bd9829b263f0c7eb4a401fb18fdb42
marcosarzuza <marcosarzuzat@gmail.com>
2025-01-30 21:08:27 -0500

    Agregar archivo de ejemplo en Python con variables y tipos de datos

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/marcosarzuza.py
@@ -0,0 +1,26 @@
+# https://www.python.org/
+'''
+Esta es una forma de hacer comentarios en python
+'''
+
+"""
+Esta es otra forma de hacer comentarios en python
+"""
+
+variable = 1995
+
+CONSTANTE = "Esto es una constante"
+
+entero = 29
+float = 1.74
+
+# Crea variables representando todos los tipos de datos primitivos
+string = "Marco Hernani"
+complejo = 1.4 + 1.7j
+lista = ["oso", "delfín", "panda"]
+tupla = ("a", "e", "i", "o", "u")
+conjunto = {2, 3, 5, 7}
+diccionario = {"Spiderman": "Peter Parker", "Batman": "Bruce Wayne", "Superman": "Clark Kent", "Pastelman": "Homer Simpson"}
+booleano = True
+
+print("¡Hola, Python!")
